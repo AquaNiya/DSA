4c47708ae170ee557451c05b4b365bce67793f9b
Soniya Bisht <bishtsoniya216@gmail.com>
2026-08-01 18:42:48 +0530

    Input-Output ques'n

--- Input_Output1.java
+++ Input_Output1.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class Input_Output1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first integer: ");
         int a = sc.nextInt();
+        System.out.print("Enter second integer: ");
         int b = sc.nextInt();
         int sum = a + b;
         System.out.println("Sum: " + sum);
