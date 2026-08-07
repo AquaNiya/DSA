d7d9550ebe2266a7dfea074f930238bcdd536bc6
Soniya Bisht <bishtsoniya216@gmail.com>
2026-08-08 00:30:21 +0530

    Input_Output Two integer sum problem

--- Input_Output1.java
+++ Input_Output1.java
@@ -1,14 +1,14 @@
 //WAP tht reads two integers from the user and prints their sum
-import java.util.Scanner;
+import java.util.Scanner; //Scanner (in java.util) reads input from System.in
 public class Input_Output1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter first integer: ");
-        int a = sc.nextInt();
+        int a = sc.nextInt(); //to input integer values
         System.out.print("Enter second integer: ");
         int b = sc.nextInt();
         int sum = a + b;
         System.out.println("Sum: " + sum);
-        sc.close();
+        sc.close(); //It's necessary to close scanner class
     }
 }
