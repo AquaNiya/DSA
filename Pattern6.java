4c95eafb2de34b6973018b52bc67edec8605beb5
Soniya Bisht <bishtsoniya216@gmail.com>
2026-08-19 00:51:21 +0530

    Pattern Problem - 6

---
+++ Pattern6.java
@@ -0,0 +1,29 @@
+class Solution {
+    // Function to print Pattern 6
+    public void pattern6(int N) {
+        // Outer loop for rows
+        for (int i = 0; i < N; i++) {
+            // Inner loop for columns
+            // Prints numbers from 1 up to (N - i)
+            for (int j = N; j > i; j--) {
+                System.out.print((N - j + 1) + " ");
+            }
+            // Move to next line
+            System.out.println();
+        }
+    }
+}
+
+public class Pattern6 {
+    public static void main(String[] args) {
+        // Create object of Solution class
+        Solution sol = new Solution();
+
+        // Define size of pattern
+        int N = 5;
+
+        // Call pattern function
+        sol.pattern6(N);
+    }
+}
+
